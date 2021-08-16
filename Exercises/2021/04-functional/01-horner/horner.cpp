ef33c36409e579f03a4ef7616731dc1482439691
Randeep Singh <randeep.singh@mail.polimi.it>
2021-08-16 17:38:28 +0000

    Solution of lab4 - ex1 - points 1. and 2.

--- Exercises/2021/04-functional/01-horner/horner.cpp
+++ Exercises/2021/04-functional/01-horner/horner.cpp
@@ -4,14 +4,38 @@
 
 double
 eval(const std::vector<double> &a, const double &x)
-{}
+{
+  double result{0.0};
+  for(std::size_t i = 0; i < a.size(); ++i)
+    result += a[i] * std::pow(x,i);
+
+  return result;
+}
 
 double
 eval_horner(const std::vector<double> &a, const double &x)
-{}
+{
+  double result{a.back()}; // a[n]
+
+  for(std::size_t i = a.size()-1; i > 0; --i)
+    result += a[i-1] + x*result;
+
+  return result;
+
+
+}
 
 std::vector<double>
 evaluate_poly(const std::vector<double> &points,
               const std::vector<double> &a,
               eval_method                method)
-{}
+{
+  std::vector<double> results(points.size());
+
+  for (std::size_t i = 0; i < points.size(); ++i)
+    results[i] = method(a, points[i]);
+
+  return results;
+
+
+}
