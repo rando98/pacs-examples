12b6a0ea3903006f9191e08d65576c69175a00b4
Randeep Singh <randeep.singh@mail.polimi.it>
2021-08-17 16:52:38 +0000

    Solved lab4-ex2-point 2: algorithm parameters read from the command line using GetPot.

--- Exercises/2021/04-functional/02-newton/main.cpp
+++ Exercises/2021/04-functional/02-newton/main.cpp
@@ -1,16 +1,25 @@
 #include "newton.hpp"
 #include <iostream>
 
+#include "GetPot"
+
 int
 main(int argc, char **argv)
 {
   std::function <double (double)> fun = [](double x) {return x*x*x + 5*x + 3;};
   std::function <double (double)> dfun = [](double x) {return 3*x*x + 5;};
 
+  GetPot parser(argc, argv);
 
-  newton_solver solver(fun, dfun, 1000, 1e-6);
+  std::size_t n_max = parser("n_max", 1000);
+  double tol = parser("tol", 1e-6);
+  double x0 = parser("x0", 0.0);
 
-  auto [x, iter] = solver.solve(0.0); // structured binding
+  std::cout << n_max << '\n';
+
+  newton_solver solver(fun, dfun, n_max, tol);
+
+  auto [x, iter] = solver.solve(x0); // structured binding
 
   std::cout << "Solution = " << x << std::endl;
   std::cout << "Iterations = " << iter << std::endl;
