1fec7c547a2068812b7cbcc52d9162efaaa37714
Randeep Singh <randeep.singh@mail.polimi.it>
2021-08-17 16:41:53 +0000

    Substituted fun and dfun with lambda expressions in the int main()

--- Exercises/2021/04-functional/02-newton/main.cpp
+++ Exercises/2021/04-functional/02-newton/main.cpp
@@ -1,21 +1,13 @@
 #include "newton.hpp"
 #include <iostream>
 
-double
-fun(double x)
-{
-  return x*x*x + 5*x + 3;
-}
-
-double
-dfun(double x)
-{
-  return 3*x*x + 5;
-}
-
 int
 main(int argc, char **argv)
 {
+  std::function <double (double)> fun = [](double x) {return x*x*x + 5*x + 3;};
+  std::function <double (double)> dfun = [](double x) {return 3*x*x + 5;};
+
+
   newton_solver solver(fun, dfun, 1000, 1e-6);
 
   auto [x, iter] = solver.solve(0.0); // structured binding
