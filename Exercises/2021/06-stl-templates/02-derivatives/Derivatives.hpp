3d37be0d0f4de0cbe6484274fdd34977ebd97941
Randeep Singh <randeep.singh@mail.polimi.it>
2021-08-21 07:09:50 +0000

    In the solution of lab6-ex2, added template argument to be able to choose between forward and backward formula, and to be able to switch beetween the two formula in the calculus of high order derivative (more stability)

--- Exercises/2021/06-stl-templates/02-derivatives/Derivatives.hpp
+++ Exercises/2021/06-stl-templates/02-derivatives/Derivatives.hpp
@@ -3,7 +3,21 @@
 
 #include<functional>
 
-template <unsigned int N, class FunctionType>
+class BACKWARD; //
+
+class FORWARD
+{
+public:
+  using Other = BACKWARD; // SO I AM ABLE TO SWITCH BEETWEEN FORWARD AND BACKWARD WHILE CALCULATING THE ORDERS OF DERIVATIVE!
+};
+
+class BACKWARD
+{
+public:
+  using Other = FORWARD;
+};
+
+template <unsigned int N, class FunctionType, class DifferenceType>
 class NthDerivative
 {
 public:
@@ -15,16 +29,19 @@ public:
   double
   operator()(const double& x) const
   {
-    return previous_derivative(x+h) - previous_derivative((x)) / h;
+    if constexpr (std::is_same_v<DifferenceType, FORWARD>)
+      return (previous_derivative(x+h) - previous_derivative(x)) / h;
+    else
+      return (previous_derivative(x) - previous_derivative(x-h)) / h;
   }
 private:
-  NthDerivative<N-1, FunctionType> previous_derivative;
+  NthDerivative<N-1, FunctionType, typename DifferenceType::Other> previous_derivative;
   double h;
 };
 
 // Specialization for N=1
-template<class FunctionType>
-class NthDerivative<1, FunctionType>
+template<class FunctionType, class DifferenceType>
+class NthDerivative<1, FunctionType, DifferenceType >
 {
 public:
   NthDerivative(const FunctionType& f, const double& h)
@@ -34,7 +51,10 @@ public:
   double
   operator()(const double& x) const
   {
-    return (f(x+h)-f(x)) / h;
+  if constexpr (std::is_same_v<DifferenceType, FORWARD>)
+    return (f(x+h) - f(x)) / h;
+  else
+    return (f(x) - f(x-h)) / h;
   }
 
 private:
@@ -46,7 +66,15 @@ template<unsigned int N, class FunctionType>
 auto // NthDerivative<N, FunctionType>
 make_forward_derivative(const FunctionType& f,const double& h)
 {
-  return NthDerivative<N, FunctionType>(f, h);
+  return NthDerivative<N, FunctionType, FORWARD>(f, h);
 }
+
+template<unsigned int N, class FunctionType>
+auto // NthDerivative<N, FunctionType>
+make_backward_derivative(const FunctionType& f,const double& h)
+{
+  return NthDerivative<N, FunctionType, BACKWARD>(f, h);
+}
+
 
 #endif /* DERIVATIVES_HPP */
--- Exercises/2021/06-stl-templates/02-derivatives/main.cpp
+++ Exercises/2021/06-stl-templates/02-derivatives/main.cpp
@@ -13,11 +13,11 @@ main(int argc, char **argv)
     return std::pow(x, 4);
   };
 
-  auto df = make_forward_derivative<4, decltype(f)>(f, h);
+  const auto df = make_forward_derivative<4>(f, h);
   std::cout << "Approx. 4th derivative of x^4 at x = 5 is " << df(5.) << "."
             << std::endl;
 
-  // const auto g = [](const double &x) { return std::exp(x); };
-  // std::cout << "Approx. 3rd derivative of exp(x) at x = 2 is "
-  //           << NthDerivative<3, decltype(g)>{g, h}(2.) << "." << std::endl;
+  const auto g = [](const double &x) { return std::exp(x); };
+  std::cout << "Approx. 3rd derivative of exp(x) at x = 2 is "
+            << NthDerivative<3, decltype(g), BACKWARD>{g, h}(2.) << "." << std::endl;
 }
