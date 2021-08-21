f203f662d3a7130a40b121f8789dc6b80a0926c7
Randeep Singh <randeep.singh@mail.polimi.it>
2021-08-21 06:52:10 +0000

    Added solution lab6-ex2, first version usign only forward derivatives

--- Exercises/2021/06-stl-templates/02-derivatives/Derivatives.hpp
+++ Exercises/2021/06-stl-templates/02-derivatives/Derivatives.hpp
@@ -3,44 +3,50 @@
 
 #include<functional>
 
-template <unsigned int N>
+template <unsigned int N, class FunctionType>
 class NthDerivative
 {
 public:
 
-  NthDerivative(const NthDerivative<N-1>& f, const double& h)
-    : previous_derivative(f), h(h)
+  NthDerivative(const FunctionType& f, const double& h)
+    : previous_derivative(f, h), h(h)
   {}
 
   double
-  operator()(const double& x)
+  operator()(const double& x) const
   {
     return previous_derivative(x+h) - previous_derivative((x)) / h;
   }
 private:
-  NthDerivative<N-1> previous_derivative;
+  NthDerivative<N-1, FunctionType> previous_derivative;
   double h;
 };
 
 // Specialization for N=1
-template<>
-class NthDerivative<1>
+template<class FunctionType>
+class NthDerivative<1, FunctionType>
 {
 public:
-  NthDerivative(const std::function<double(double)>& f, const double& h)
+  NthDerivative(const FunctionType& f, const double& h)
     : f(f), h(h)
   {};
 
   double
-  operator()(const double& x)
+  operator()(const double& x) const
   {
     return (f(x+h)-f(x)) / h;
   }
 
 private:
-  std::function<double(double)> f;
+  FunctionType f;
   double h;
 };
 
+template<unsigned int N, class FunctionType>
+auto // NthDerivative<N, FunctionType>
+make_forward_derivative(const FunctionType& f,const double& h)
+{
+  return NthDerivative<N, FunctionType>(f, h);
+}
 
 #endif /* DERIVATIVES_HPP */
