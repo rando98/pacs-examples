44f6ab5634afafbe599131ad496ad97a36e4d130
Randeep Singh <randeep.singh@mail.polimi.it>
2021-08-26 10:18:02 +0000

    Added some comments on std::forward and universal binding

--- Exercises/2021/07-eigen-newton/src/JacobianFactory.hpp
+++ Exercises/2021/07-eigen-newton/src/JacobianFactory.hpp
@@ -13,12 +13,12 @@ enum class JacobianType
 
 template<ProblemType ProbType, JacobianType Type, class... Args> // variadic template, used because Discrete and FullJacobian uses different constructors parameters.
 std::unique_ptr<JacobianBase<ProbType>>
-make_jacobian(Args&&... args)
+make_jacobian(Args&&... args) // universal reference for Args, since type of Args is deduced at the moment of instance of the method  !!!!
 {
   if constexpr (Type == JacobianType::Discrete)
-    return std::make_unique<DiscreteJacobian<ProbType>>(std::forward<Args>(args)...);
+    return std::make_unique<DiscreteJacobian<ProbType>>(std::forward<Args>(args)...); // forwarding the same value category of Args passed to the template
   else if constexpr (Type == JacobianType::Full)
-    return std::make_unique<FullJacobian<ProbType>>(std::forward<Args>(args)...);
+    return std::make_unique<FullJacobian<ProbType>>(std::forward<Args>(args)...); // forwarding ...
 }
 
 #endif /* JACOBIANFACTORY_HPP */
